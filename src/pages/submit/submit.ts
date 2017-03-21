79344dd82799edfd66db1e3c215a48adc061cfca
최규민 <choigyumin@gmail.com>
2017-03-21 17:44:03 +0900

    submit page db connect && file deletion

--- src/pages/submit/submit.ts
+++ src/pages/submit/submit.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
 
-import { NavController, AlertController, ToastController, Events } from 'ionic-angular';
+import { Nav, NavController, AlertController, ToastController, Events } from 'ionic-angular';
+
+import { Database, User,Auth } from '@ionic/cloud-angular';
+import { Statistics } from '../statistics/statistics';
 
 @Component({
   selector: 'page-submit',
   templateUrl: 'submit.html'
 })
 export class Submit {
+    
+  submit = {date:'', box:'', wod:'', record:'', memo:''};
+
+  //TODO: 모든페이지에 MORE 토스트버튼 추가
   public color: string = 'primary';
   public position: string = 'right';
   public icon: string = 'more';
   public enableBackdropDismiss: boolean = false;
   public buttonColor: string = 'dark';
-  constructor(public navCtrl: NavController, private alertCtrl: AlertController, public toastCtrl: ToastController, public events:Events) {
-      this.events.subscribe('user:logged_in', () => { 
-        this.presentToast('this works??');
-      });
+  constructor(public nav:Nav, public navCtrl: NavController, private alertCtrl: AlertController, public toastCtrl: ToastController, public events:Events, public db:Database, public user:User, public auth:Auth) {
+      this.db.connect();
+  }
+
+  doSubmit(form) {
+      if (form.valid) {
+          this.db.collection("users").upsert(this.submit);
+          this.nav.setRoot(Statistics);
+      }
   }
   public buttons =  [
       {
